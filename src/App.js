ca631a69220d501667824d6bd5309a509b4591fc
rgrace9 <rgracewilliams@gmail.com>
2020-08-23 19:18:24 -0400

    Add folders to project and react router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import cities from "./api/citiesData";
-import { v4 as uuidv4 } from "uuid";
+import cities from "./api/cities";
+import { Button } from "@material-ui/core";
 
 function App() {
-  let list = [];
-  cities.forEach((c) => {
-    const city = {};
-    city.name = c[0];
-    city.type = c[1];
-    city.county = c[2];
-    city.governmentForm = c[3];
-    city.id = uuidv4();
-
-    list.push(city);
-  });
-
-  console.log({ list });
-
-  debugger;
   return (
     <div className="App">
       <header className="App-header">
+        <Button color="primary">Hello World</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        {list.map((l) => (
+        {cities.map((l) => (
           <p key={l.id}>{l.name}</p>
         ))}
         <a
