2a024dc0f0bf9d416edb6d5eeab4eba3cec7375e
rgrace9 <rgracewilliams@gmail.com>
2020-08-23 15:47:19 -0400

    Add the formatted cities data to the repo.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import cities from "./api/citiesData";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  let list = [];
+  cities.forEach((c) => {
+    const city = {};
+    city.name = c[0];
+    city.type = c[1];
+    city.county = c[2];
+    city.governmentForm = c[3];
+    city.id = uuidv4();
+
+    list.push(city);
+  });
+
+  console.log({ list });
+
+  debugger;
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +27,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {list.map((l) => (
+          <p key={l.id}>{l.name}</p>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
