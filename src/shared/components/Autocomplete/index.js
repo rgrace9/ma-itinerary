9624d5129ef368d71ec598fe8a70798d371c0a57
rgrace9 <rgracewilliams@gmail.com>
2020-08-29 23:28:13 -0400

    Add throttle to autocomplete.

--- src/shared/components/Autocomplete/index.js
+++ src/shared/components/Autocomplete/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import PropTypes from "prop-types";
+import { debounce, throttle } from "lodash";
 import "./Autocomplete.scss";
 
 const options = [
@@ -17,23 +18,71 @@ const Autocomplete = (props) => {
   const [filteredOptions, setFilteredOptions] = useState([]);
   const [showOptions, setShowOptions] = useState(false);
 
-  const handleChange = (e) => {
-    const { value } = e.target;
+  const debounceHandler = useMemo(
+    () =>
+      debounce((value) => {
+        console.log("debounce", value);
+      }, 1000),
+    []
+  );
+  // const debounceHandler = useMemo(
+  //   () =>
+  //     debounce((value) => {
+  //       console.log(`Debounced Value = ${value}`);
+  //     }, 1000),
+  //   []
+  // );
 
-    const filtered = options.filter((opt) => {
-      return opt.toLowerCase().indexOf(userInput.toLowerCase()) > -1;
-    });
+  const throttleHandler = useMemo(
+    () =>
+      throttle((value) => {
+        console.log("throttle", value);
+        const filtered = options.filter((opt) => {
+          return opt.toLowerCase().indexOf(value.toLowerCase()) > -1;
+        });
+        console.log("filtered", filtered);
 
-    setUserInput(value);
-    setFilteredOptions(filtered);
-    setActiveOption(0);
-    setShowOptions(true);
-  };
+        setUserInput(value);
+        setFilteredOptions(filtered);
+        setActiveOption(0);
+        setShowOptions(true);
+      }),
+    []
+  );
 
+  // const handleChange = throttle(
+  //   500,
+  //   (e) => {
+  //     const { value } = e.target;
+
+  //     const filtered = options.filter((opt) => {
+  //       return opt.toLowerCase().indexOf(userInput.toLowerCase()) > -1;
+  //     });
+
+  //     setUserInput(value);
+  //     setFilteredOptions(filtered);
+  //     setActiveOption(0);
+  //     setShowOptions(true);
+  //   },
+  //   1000
+  // );
+
+  const handleChange = useCallback(
+    (e) => {
+      const { value } = e.target;
+      debounceHandler(value);
+      throttleHandler(value);
+      setUserInput(value);
+      // setFilteredOptions(filtered);
+      setActiveOption(0);
+      setShowOptions(true);
+    },
+    [debounceHandler, throttleHandler]
+  );
   console.log({ userInput });
 
   const handleClick = (e) => {
-    console.log("click", e.currentTarget.innerText);
+    // console.log("click", e.currentTarget.innerText);
     setUserInput(e.currentTarget.innerText);
     setFilteredOptions([]);
     setActiveOption(0);
@@ -42,7 +91,7 @@ const Autocomplete = (props) => {
 
   const handleKeyDown = (e) => {
     const { keyCode } = e;
-    console.log({ keyCode, e });
+    // console.log({ keyCode, e });
     if (keyCode === 13) {
       setActiveOption(0);
       setShowOptions(false);
@@ -60,17 +109,46 @@ const Autocomplete = (props) => {
     }
   };
 
-  console.log({ filteredOptions });
+  // console.log({ filteredOptions });
+  let optionList;
+  if (showOptions && userInput) {
+    if (filteredOptions.length) {
+      optionList = (
+        <ul className="autocomplete__options">
+          {filteredOptions.map((optionName, index) => {
+            let className;
+            if (index === activeOption) {
+              className = "autocomplete__option--active";
+            }
+            return (
+              <li className={className} key={optionName} onClick={handleClick}>
+                {optionName}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      optionList = (
+        <div className="autocomplete__no-options">
+          <em>No Option!</em>
+        </div>
+      );
+    }
+  }
 
   return (
-    <input
-      className="autocomplete"
-      type="text"
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-      onClick={handleClick}
-      value={userInput}
-    />
+    <div className="autocomplete-container">
+      <input
+        className="autocomplete"
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        onClick={handleClick}
+        value={userInput}
+      />
+      {optionList}
+    </div>
   );
 };
 
